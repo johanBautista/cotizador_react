a8058d4d0edd9f5c66997cb14c8934c407040273
johan Bautista <jsbparra@gmail.com>
2020-05-10 11:29:38 +0200

    import transition group

--- src/Components/Resultado.js
+++ src/Components/Resultado.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const Mensaje = styled.p`
   background-color: #20b2aa67;
@@ -10,11 +11,29 @@ const Mensaje = styled.p`
   text-transform: uppercase;
 `;
 
+const Contenedor = styled.div`
+  background-color: #20b2aa67;
+  text-align: center;
+  text-transform: uppercase;
+  text-align: center;
+  position: relative;
+`;
+
 const Resultado = ({ cotizacion }) => {
   return cotizacion === 0 ? (
     <Mensaje>Elige los datos del seguro</Mensaje>
   ) : (
-    <Mensaje>Tu seguro es: $ {cotizacion}</Mensaje>
+    <Contenedor>
+      <TransitionGroup component="p" className="resultado">
+        <CSSTransition
+          classNames="resultado"
+          key={cotizacion}
+          timeout={{ enter: 500, exit: 500 }}
+        >
+          <Mensaje>Tu seguro es: $ {cotizacion}</Mensaje>
+        </CSSTransition>
+      </TransitionGroup>
+    </Contenedor>
   );
 };
 
